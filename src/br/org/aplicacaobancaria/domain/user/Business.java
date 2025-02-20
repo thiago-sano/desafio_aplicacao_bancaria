7cb11f058a44ce615f6373e2d732dcec895778b7
Thiago Sano <thiago.sano@outlook.com>
2025-02-19 21:38:01 -0300

    feat: implemented toString - Business/Personal

--- src/br/org/aplicacaobancaria/domain/user/Business.java
+++ src/br/org/aplicacaobancaria/domain/user/Business.java
@@ -21,4 +21,9 @@ public class Business extends Client{
     public ClientType getClientType() {
         return clientType;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %-15s | Email: %-30s | CPF/CNPJ: %-16s | Tipo: %-10s | Capital: %,.2f", getName(), getEmail(), getId(), getClientType().getNome(), getStatedCapital());
+    }
 }
--- src/br/org/aplicacaobancaria/domain/user/Personal.java
+++ src/br/org/aplicacaobancaria/domain/user/Personal.java
@@ -11,4 +11,9 @@ public class Personal extends Client{
     public ClientType getClientType() {
         return clientType;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Nome: %-15s | Email: %-30s | CPF/CNPJ : %-15s | Tipo: %-10s", getName(), getEmail(), getId(), getClientType().getSigla());
+    }
 }
