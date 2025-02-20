c3adfac3d0aa6a6e9721b29779b2d4b5153800b0
Thiago Sano <thiago.sano@outlook.com>
2025-02-20 09:16:44 -0300

    feat: UI print ClientType options

--- src/br/org/aplicacaobancaria/application/UI.java
+++ src/br/org/aplicacaobancaria/application/UI.java
@@ -1,7 +1,9 @@
 package br.org.aplicacaobancaria.application;
 
+import br.org.aplicacaobancaria.domain.bank.Account;
 import br.org.aplicacaobancaria.domain.user.Business;
 import br.org.aplicacaobancaria.domain.user.Client;
+import br.org.aplicacaobancaria.domain.user.ClientType;
 import br.org.aplicacaobancaria.domain.user.Personal;
 
 import java.util.Scanner;
@@ -12,49 +14,47 @@ public class UI {
     }
 
     public static int printMainMenu() {
-        clearScreen();
         int option;
+
         System.out.println("MENU PRINCIPAL");
         System.out.println("1 - CADASTRO DE CLIENTE");
         System.out.println("2 - LISTAR CLIENTES");
-        System.out.println("3 - SAIR");
+        System.out.println("3 - CADASTRO DE CONTA");
+        System.out.println("4 - LISTAR CONTAS");
+        System.out.println("5 - SAIR");
         System.out.print("Digite uma opcao: ");
         option = readMenuOption();
         return option;
     }
 
-    // https://stackoverflow.com/questions/2979383/java-clear-the-console
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
-
     public static Client printRegisterClient(){
         Scanner sc = new Scanner(System.in);
 
-        clearScreen();
         System.out.print("NOME DO CLIENTE: ");
-        String clientName = sc.next();
+        String clientName = sc.next().toUpperCase();
         System.out.print("EMAIL DO CLIENTE: ");
-        String clientEmail = sc.next();
+        String clientEmail = sc.next().toUpperCase();
 
         while (true){
-            System.out.println("1 - PESSOA FISICA");
-            System.out.println("2 - PESSOA JURIDICA");
-            System.out.print("INFORME O TIPO DE CONTA (1/2): ");
+            // print com numeracao e utilizacao da classe enum
+            System.out.println();
+            for(ClientType ct : ClientType.values()){
+                System.out.println(ct.getMenuName());
+            }
+            System.out.print("INFORME O NUMERO: ");
             int option = readMenuOption();
             switch (option){
                 case 1:{
                     System.out.print("CPF (somente numeros): ");
                     String clientId = sc.next();
-                    return new Personal(clientName, clientEmail, clientId);
+                    return new Personal(clientName, clientEmail, clientId, ClientType.PERSONAL);
                 }
                 case 2:{
                     System.out.print("CNPJ (somente numeros): ");
                     String clientId = sc.next();
                     System.out.print("CAPITAL DECLARADO: ");
                     Double clientStatedCapital = sc.nextDouble();
-                    return new Business(clientName, clientEmail,clientId, clientStatedCapital);
+                    return new Business(clientName, clientEmail,clientId, ClientType.BUSINESS, clientStatedCapital);
                 }
                 default:{
                     System.out.println("-> OPCAO INVALIDA");
@@ -63,6 +63,22 @@ public class UI {
             }
         }
     }
+
+//    public static Account printRegisterAccount(){
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("NUMERO DA AGENCIA: ");
+//        String branchNumber = sc.next();
+//        System.out.print("NUMERO DA CONTA: ");
+//        String accountNumber = sc.next();
+//        Double balance = 0.0;
+//        //client
+//
+//        //AccountType
+//
+//
+//    }
+
 
     public static int readMenuOption(){
         Scanner sc = new Scanner(System.in);
